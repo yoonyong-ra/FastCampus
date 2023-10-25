89bd6d4ece8522dfcd2d5d26fc4dc3902864734c
yoonyong ra <yyna@naver.com>
2023-10-25 12:44:28 +0900

    불필요 파일 정리

--- IaC/terraform-codes/internet_gateway.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_internet_gateway" "terra-internet-gateway" {
-
-  depends_on = [
-    aws_vpc.terra-vpc
-  ]
-
-  vpc_id = aws_vpc.terra-vpc.id
-}
--- IaC/terraform-codes/route_table_association.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_route_table_association" "terra-route-association-pub-sub1" {
-  route_table_id = aws_route_table.terra-route-table-pub-sub1.id
-  subnet_id      = aws_subnet.terra-public-subnet1.id
-}
-
-resource "aws_route_table_association" "terra-route-association-pub-sub3" {
-  route_table_id = aws_route_table.terra-route-table-pub-sub3.id
-  subnet_id      = aws_subnet.terra-public-subnet3.id
-}
